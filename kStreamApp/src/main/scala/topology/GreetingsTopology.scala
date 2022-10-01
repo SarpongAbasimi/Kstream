4b9c6fdbab42843805347191c5bf44c666cdf9a4
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2022-10-01 16:57:30 +0100

    Learned how to branch streams of data

--- kStreamApp/src/main/scala/topology/GreetingsTopology.scala
+++ kStreamApp/src/main/scala/topology/GreetingsTopology.scala
@@ -2,10 +2,10 @@ package topology
 import models.Greetings
 import org.apache.kafka.streams.Topology
 import org.apache.kafka.streams.scala.StreamsBuilder
-import org.apache.kafka.streams.scala.kstream.KStream
-import org.apache.kafka.streams.kstream.Consumed
+import org.apache.kafka.streams.scala.kstream.{Branched, KStream}
+import org.apache.kafka.streams.kstream.{Consumed, Named}
 import serde.CustomSerde
-import models.Greetings.{GoodAfternoon, GoodMorning}
+import models.Greetings.{GoodAfternoon, GoodEvening, GoodMorning}
 import org.apache.kafka.streams.scala.serialization.Serdes
 
 object GreetingsTopology {
@@ -25,15 +25,70 @@ object GreetingsTopology {
 
     val filteredStream = kStream.filterNot((_, value: Greetings) => {
       value match {
-        case GoodMorning(to) if to.toLowerCase == "chris" | to.toLowerCase == "ben" =>
-          println(s"Skipping ${value.toString} with name $to"); true
-        case GoodAfternoon(_) => println(s"Skipping GoodAfternoon"); true
-        case _                => println(s"Bingo 🎉"); false
+        case GoodMorning(to)
+            if to.toLowerCase
+              == "chris" |
+              to.toLowerCase == "ben" =>
+          true
+        case GoodAfternoon(_) => true
+        case _                => false
       }
     })
 
-    filteredStream.foreach { (k, v) =>
-      println(s"The key is  $k and value is $v")
+    val theBranch = filteredStream
+      .split(
+        Named.as(
+          "Greet-"
+        )
+      )
+      .branch(
+        (_, v) =>
+          v match {
+            case GoodMorning(to) if to.toLowerCase.startsWith("s") => true
+            case _                                                 => false
+          },
+        Branched.as("GoodMorning")
+      )
+      .branch(
+        (_, v) =>
+          v match {
+            case GoodEvening(to) if to.toLowerCase.startsWith("v") => true
+            case _                                                 => false
+          },
+        Branched.as("GoodEvening")
+      )
+      .defaultBranch(Branched.as("Others"))
+
+    // Getting data for each branch
+    val getBranchGoodMorning: Option[KStream[String, Greetings]] =
+      theBranch.get("Greet-GoodMorning")
+
+    val getBranchEvening: Option[KStream[String, Greetings]] =
+      theBranch.get("Greet-GoodEvening")
+
+    val getOthers: Option[KStream[String, Greetings]] =
+      theBranch.get("Greet-Others")
+
+    // Printing data from branch
+    getBranchGoodMorning.collect { case value =>
+      value.foreach { (k, v) =>
+        println("Learning about Branches in KafkaStream! -> getBranchGoodMorning")
+        println(s"The key is $k and the value is ${v}")
+      }
+    }
+
+    getBranchEvening.collect { case value =>
+      value.foreach { (k, v) =>
+        println("Learning about Branches in KafkaStream! -> getBranchEvening")
+        println(s"The key is $k and the value is ${v}")
+      }
+    }
+
+    getOthers.collect { case value =>
+      value.foreach { (k, v) =>
+        println("Learning about Branches in KafkaStream! -> getOthers")
+        println(s"The key is $k and the value is ${v}")
+      }
     }
 
     streamsBuilder.build()
